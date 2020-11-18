754be96a1d38864e8e98645cc150d81a4a3aad56
Vibhanshu Garg <vibhanshugarg@Vibhanshus-MacBook-Air.local>
2020-11-18 12:44:13 +0530

    Login flutter app clone of spotify login page

---
+++ lib/main.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: Column(
+        children: [
+          Container(
+            child: Stack(
+              children: [
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                  child: Text(
+                    'Hello',
+                    style: TextStyle(
+                      fontSize: 80.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 175.0, 0.0, 0.0),
+                  child: Text(
+                    'There',
+                    style: TextStyle(
+                      fontSize: 80.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(220.0, 175.0, 0.0, 0.0),
+                  child: Text(
+                    '.',
+                    style: TextStyle(
+                      fontSize: 80.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'EMAIL',
+                    labelStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green)
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                TextField(
+                  decoration: InputDecoration(
+
+                    labelText: 'PASSWORD',
+                    labelStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green)
+                    ),
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 5.0,),
+                Container(
+                  alignment: Alignment(1.0,0.0),
+                  padding: EdgeInsets.only(top: 15.0, left: 20.0),
+                  child: InkWell(
+                    child: Text(
+                      'Forgot Password',
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Montserrat',
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+
+                  ),
+                ),
+                SizedBox(height: 40.0,),
+                Container(
+                  height: 50.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(25.0),
+                    shadowColor: Colors.greenAccent,
+                    color: Colors.green,
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: (){},
+                      child: Center(
+                        child: Text(
+                          'LOGIN',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Montserrat',
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                Container(
+                  height: 50.0,
+                  color: Colors.transparent,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        style: BorderStyle.solid,
+                        width: 1.0,
+                      ),
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(
+                            child: Image.network('https://toppng.com/uploads/preview/facebook-f-icon-facebook-logo-png-white-11563016108d1db9g68rk.png'),
+                          ),
+                        ),
+                        SizedBox(width: 10.0,),
+                        Center(
+                          child: Text('Login with Facebook',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Montserrat'
+                          ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15.0,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'New to Spotify?',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat'
+                      ),
+                    ),
+                    SizedBox(width: 5.0,),
+                    InkWell(
+                      onTap: () {},
+                      child: Text(
+                        'Register',
+                        style: TextStyle(
+                          color: Colors.green,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
